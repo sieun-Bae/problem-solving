3248d368ca0dccdc86a689ca098ab3f6dcffad67
sieun-Bae <qotldms130@gmail.com>
2020-10-16 01:28:06 +0900

    goldbach completed

--- math/primeNum.py
+++ math/primeNum.py
@@ -16,10 +16,9 @@ def prime_root(n):
 			return False
 	return True
 
-def sieveOfEratosthenes():
+def sieveOfEratosthenes(): # False: 지워진 것 (소수가 아닌 것), True: 지워지지 않음 (소수)
 	p = list()
 	c = list(range(0, 101))
-	print(c)
 	for i in range(2, 101):
 		if c[i]:
 			p.append(i)
@@ -29,4 +28,24 @@ def sieveOfEratosthenes():
 				j += i
 	print(p)
 
-sieveOfEratosthenes()
+def goldbach():
+	p = list()
+	c = list(range(0, 1000001))
+	
+	for i in range(2, 1000001):
+		if c[i]:
+			p.append(i)
+			j = i+i #정수 overflow 막기 위해 i*2로 하기도
+			while j<=1000000:
+				c[j] = False
+				j += i
+	while True:
+		n = int(input())
+		if n==0:
+			break
+		for i in range(1, len(p)):
+			if c[n-p[i]]:
+				print(n,"=",p[i],"+",n-p[i])
+				break
+
+goldbach()
